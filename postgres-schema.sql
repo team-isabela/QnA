83ae01e71c35db8059ba8356a6bc42bc82750d86
Kavita <kavi.pdri@gmail.com>
2022-01-13 12:30:57 -0800

    configuring elt file

--- postgres-schema.sql
+++ postgres-schema.sql
@@ -1,8 +1,14 @@
+DROP DATABASE IF EXISTS QnA;
+
+DROP TABLE IF EXISTS questions, answers, answers_photos;
+
+CREATE DATABASE QnA;
+
 CREATE TABLE questions (
  product_id INTEGER,
  question_id BIGSERIAL,
  question_body VARCHAR,
- question_date TIMESTAMP,
+ question_date DATE,
  asker_name VARCHAR,
  asker_email VARCHAR,
  reported BOOLEAN,
@@ -12,11 +18,15 @@ CREATE TABLE questions (
 
 ALTER TABLE questions ADD CONSTRAINT questions_pkey PRIMARY KEY (question_id);
 
+COPY questions
+FROM '/home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/questions.csv'
+DELIMITER ',' CSV HEADER;
+
 CREATE TABLE answers (
  answer_id BIGSERIAL,
  question_id INTEGER,
  answer_body VARCHAR,
- answer_date TIMESTAMP,
+ answer_date DATE,
  answerer_name VARCHAR,
  answerer_email VARCHAR,
  reported BOOLEAN,
@@ -26,14 +36,21 @@ CREATE TABLE answers (
 
 ALTER TABLE answers ADD CONSTRAINT answers_pkey PRIMARY KEY (answer_id);
 
-CREATE TABLE photos (
+COPY answers
+FROM '/home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/answers.csv'
+DELIMITER ',' CSV HEADER;
+
+CREATE TABLE answers_photos (
  photo_id BIGSERIAL,
  answer_id INTEGER,
  photo_url VARCHAR
 );
 
+COPY answers_photos
+FROM '/home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/answers_photos.csv'
+DELIMITER ',' CSV HEADER;
 
-ALTER TABLE photos ADD CONSTRAINT photos_pkey PRIMARY KEY (photo_id);
+ALTER TABLE answers_photos ADD CONSTRAINT answers_photos_pkey PRIMARY KEY (photo_id);
 
 ALTER TABLE answers ADD CONSTRAINT answers_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions(question_id);
-ALTER TABLE photos ADD CONSTRAINT photos_answer_id_fkey FOREIGN KEY (answer_id) REFERENCES answers(answer_id);
+ALTER TABLE answers_photos ADD CONSTRAINT answers_photos_answer_id_fkey FOREIGN KEY (answer_id) REFERENCES answers(answer_id);
