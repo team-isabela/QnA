b418b78053baab2f82c442cdec93160233ce137e
Kavita <kavi.pdri@gmail.com>
2022-01-14 12:31:34 -0800

    setting up base questions route

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 const postgres = require('postgres');
 
-const sql = postgres();
+const sql = postgres({
+  database: 'qna',
+  user: 'dev',
+  password: ''
+});
 
 module.exports = sql
--- server.js
+++ server.js
@@ -8,6 +8,28 @@ server.get('/', (req, res) => {
   res.send('root GET response');
 })
 
+server.get('/qa/questions', async (req, res) => {
+  console.log(req.query);
+  const questions = await sql`
+    select * from questions
+    where product_id = ${req.query.product_id}
+    limit ${req.query.count || 5}
+  `
+  questions.forEach(async question => {
+    // console.log(question);
+    const answers = await sql`
+    select * from answers
+    where question_id = ${question.question_id}
+    `
+    // console.log(answers);
+    // console.log(question.answers);
+    question.answers = answers;
+    // console.log(question.answers);
+  })
+  console.log(questions.answers);
+  res.send(questions);
+})
+
 server.listen(port, () => {
   console.log(`sdc6 qna server.js now listening at port ${port}...`)
 })
