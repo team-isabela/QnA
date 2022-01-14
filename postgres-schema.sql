9952f909db77af09ce616d9026b6493f3094c91f
Kavita <kavi.pdri@gmail.com>
2022-01-13 16:15:55 -0800

    working test implementation

--- postgres-schema.sql
+++ postgres-schema.sql
@@ -1,32 +1,37 @@
-DROP DATABASE IF EXISTS QnA;
-
-DROP TABLE IF EXISTS questions, answers, answers_photos;
+DROP DATABASE IF EXISTS QnA; --will throw an error if you're already on qna but it's fine
 
 CREATE DATABASE QnA;
 
+\c qna;
+
+DROP TABLE IF EXISTS questions, answers, answers_photos;
+
 CREATE TABLE questions (
- product_id INTEGER,
  question_id BIGSERIAL,
+ product_id INTEGER,
  question_body VARCHAR,
- question_date DATE,
+ question_date BIGINT,
  asker_name VARCHAR,
  asker_email VARCHAR,
  reported BOOLEAN,
  question_helpfulness INTEGER
 );
 
-
 ALTER TABLE questions ADD CONSTRAINT questions_pkey PRIMARY KEY (question_id);
 
 COPY questions
-FROM '/home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/questions.csv'
+FROM PROGRAM 'head -1000 /home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/questions.csv'
 DELIMITER ',' CSV HEADER;
+
+UPDATE questions SET question_date = question_date/1000;
+ALTER TABLE questions
+ALTER COLUMN question_date TYPE TIMESTAMP USING to_timestamp(question_date);
 
 CREATE TABLE answers (
  answer_id BIGSERIAL,
  question_id INTEGER,
  answer_body VARCHAR,
- answer_date DATE,
+ answer_date BIGINT,
  answerer_name VARCHAR,
  answerer_email VARCHAR,
  reported BOOLEAN,
@@ -37,8 +42,12 @@ CREATE TABLE answers (
 ALTER TABLE answers ADD CONSTRAINT answers_pkey PRIMARY KEY (answer_id);
 
 COPY answers
-FROM '/home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/answers.csv'
+FROM PROGRAM 'head -1000 /home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/answers.csv'
 DELIMITER ',' CSV HEADER;
+
+UPDATE answers SET answer_date = answer_date/1000;
+ALTER TABLE answers
+ALTER COLUMN answer_date TYPE TIMESTAMP USING to_timestamp(answer_date);
 
 CREATE TABLE answers_photos (
  photo_id BIGSERIAL,
@@ -47,7 +56,7 @@ CREATE TABLE answers_photos (
 );
 
 COPY answers_photos
-FROM '/home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/answers_photos.csv'
+FROM PROGRAM 'head -1000 /home/kpoduri/projects/hackreactor/senior-phase/sdc6/csv-data/answers_photos.csv'
 DELIMITER ',' CSV HEADER;
 
 ALTER TABLE answers_photos ADD CONSTRAINT answers_photos_pkey PRIMARY KEY (photo_id);
