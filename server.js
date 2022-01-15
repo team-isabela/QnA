3694637c6ebfbb038e9efde1385f2f91ea04fe56
Kavita <kavi.pdri@gmail.com>
2022-01-14 16:03:31 -0800

    get /qa/questions

--- server.js
+++ server.js
@@ -9,25 +9,47 @@ server.get('/', (req, res) => {
 })
 
 server.get('/qa/questions', async (req, res) => {
-  console.log(req.query);
   const questions = await sql`
-    select * from questions
+    select
+      question_id,
+      question_body,
+      question_date,
+      asker_name,
+      question_helpfulness,
+      reported
+    from questions
     where product_id = ${req.query.product_id}
     limit ${req.query.count || 5}
   `
-  questions.forEach(async question => {
-    // console.log(question);
+  for (let question of questions) {
+    question.answers = {};
     const answers = await sql`
-    select * from answers
-    where question_id = ${question.question_id}
+      select
+        answer_id as id,
+        answer_body as body,
+        answer_date as date,
+        answerer_name,
+        answer_helpfulness as helpfulness
+      from answers
+      where question_id = ${question.question_id}
     `
-    // console.log(answers);
-    // console.log(question.answers);
-    question.answers = answers;
-    // console.log(question.answers);
-  })
-  console.log(questions.answers);
-  res.send(questions);
+    for (let answer of answers) {
+      console.log(answer.answe);
+      const photos = await sql`
+        select
+          photo_id as id,
+          photo_url as url
+        from answers_photos
+        where answer_id = ${answer.id}`
+      console.log(photos);
+      answer.photos = photos;
+      question.answers[answer.id] = answer;
+    }
+  }
+  res.send({
+    product_id: req.query.product_id,
+    results: questions
+  });
 })
 
 server.listen(port, () => {
